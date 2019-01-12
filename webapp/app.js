33e9a6a59e320bac5e0deb351e88496306b23348
Chris Carrick-2 <car17059@byui.edu>
2019-01-12 16:58:53 -0700

    Commited base design of web app

--- webapp/app.js
+++ webapp/app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     searchSuccesfull: false,
-    name: "Chris"
+    name: "Chris",
+    numItems: 10,
+    items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   },
   methods: {},
   computed: {}
