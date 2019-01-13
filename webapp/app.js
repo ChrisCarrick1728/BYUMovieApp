85a47a307d0904f5d9666ac65ac1a804e7dce2f7
Chris Carrick-2 <car17059@byui.edu>
2019-01-13 00:07:15 -0700

    Commiting working frontend

--- webapp/app.js
+++ webapp/app.js
@@ -2,11 +2,37 @@ new Vue({
   el: '#vue-app',
   data: {
     searchSuccesfull: false,
-    name: "Chris",
-    numItems: 10,
-    items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    searchString: '',
+    responseText: '',
+    moviesObject: [{'poster_img_url':'','title':'','popularity_summary':''}]
   },
-  methods: {},
-  computed: {}
-
+  methods: {
+    getResults: function() {
+      if (this.searchString != '') {
+        try {
+          fetch("http://localhost:9000/search/" + this.searchString)
+            .then(response => response.json())
+            .then((data) => {
+              this.moviesObject = data;
+              this.searchSuccesfull = true
+          })
+        } catch (e) {
+          console.log("error: " + e);
+        }
+      } else {
+        this.searchSuccesfull = false
+      }
+    },
+    keyPressed: function(event) {
+      console.log(event)
+      if (event.keyCode == 13) {
+        this.getResults()
+      } else if (event.keyCode == 27) {
+        this.searchString = ''
+      }
+    }
+  },
+  computed: {
+    //empty
+  }
 });
