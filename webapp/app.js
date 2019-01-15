6fe6aa9c1b0d61a53c1d70b80690376e8ff8cbc9
Chris Carrick-2 <car17059@byui.edu>
2019-01-14 20:47:52 -0700

    Fixed some design and layout issues to make responsive.

--- webapp/app.js
+++ webapp/app.js
@@ -1,41 +1,40 @@
 new Vue({
   el: '#vue-app',
   data: {
+    // searchSuccesfull: used to show/hide results screen
     searchSuccesfull: false,
-    searchString: '',
-    responseText: '',
-    moviesObject: [{'poster_img_url':'','title':'','popularity_summary':''}]
-  },
-  mounted() {
 
+    // searchString: bound to input text box to store user input
+    searchString: '',
+
+    // moviesObject: JavaScript object to contain our search results
+    moviesObject: [{'movie_id': '', 'poster_img_url':'','title':'','popularity_summary':''}]
   },
   methods: {
+
+    // getResults: function that is run when user clicks search button/
     getResults: function() {
+      // verify that the search string is not empty
       if (this.searchString != '') {
-        try {
-          fetch("http://localhost:9000/movies?search=" + this.searchString)
-            .then(response => response.json())
-            .then((data) => {
-              this.moviesObject = data;
-              this.searchSuccesfull = true
-          })
-        } catch (e) {
-          console.log("error: " + e);
-        }
+        // Perform async connection to webservice
+        fetch("http://localhost:9000/movies?search=" + encodeURI(this.searchString))
+          .then(response => response.json())
+          .then((data) => {
+            this.moviesObject = data;
+            this.searchSuccesfull = true })
       } else {
         this.searchSuccesfull = false
       }
     },
+    // keyPressed: function to add esc and enter key functionallity
     keyPressed: function(event) {
-      console.log(event)
+      // keyCode: 13 = "return" key and calls the getResults function
       if (event.keyCode == 13) {
         this.getResults()
+      // keyCode: 27 = "esc" and clears our searchString
       } else if (event.keyCode == 27) {
         this.searchString = ''
       }
     }
   },
-  computed: {
-    //empty
-  }
 });
